85c8fcd2f4b61a46ae60358937825af88bd7fc3d
Ubuntu <ubuntu@ip-172-31-86-174.ec2.internal>
2021-11-25 04:42:03 +0000

    created a basic_math.py and area_per_rect.py

---
+++ basic_math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Ketia Gaelle Kaze
+# Created on: Nov. 24, 2021
+# This program does basic math.
+
+def main():
+    print("5 + 3 = {}". format(5+3))
+    print("4 - 6 = {}". format(4-6))
+    print("6 / 3 = {}". format(6/3))
+    print("8 - 4 / 4 = {}". format(8-4/4))
+    print("5^3 = {}". format(5**3))
+    print("1 / 6 = {}". format(1/6))
+
+
+if __name__ == "__main__":
+    main()
